855505b2aa74ccf9ea638b2f5ffec25e54132277
audronf <federicofasitella@gmail.com>
2024-10-05 11:33:01 -0300

    feat: add some styles and code cleaup

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,10 +1,12 @@
 import H5AudioPlayer from "react-h5-audio-player";
+import "./AudioPlayer.css";
 import "react-h5-audio-player/lib/styles.css";
 
 export const AudioPlayer = ({ name, sound }) => {
   return (
     <div className="audio-player">
-      <H5AudioPlayer autoPlay={false} src={sound} />;
+      <h4 className="audio-name text">{name}</h4>
+      <H5AudioPlayer autoPlay={false} src={sound} />
     </div>
   );
 };
